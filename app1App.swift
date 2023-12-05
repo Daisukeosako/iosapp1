aad8dd2c3fa6cf03c66fff95019fab73bc6e1927
Daisukeosako <daisukeosako112@gmail.com>
2023-12-05 23:41:36 +0900

    First commit

---
+++ app1App.swift
@@ -0,0 +1,17 @@
+//
+//  app1App.swift
+//  app1
+//
+//  Created by 尾迫大輔 on 2023/12/05.
+//
+
+import SwiftUI
+
+@main
+struct app1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
