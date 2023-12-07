97825edb6deb800846bfb91b261180730e989034
Daisukeosako <daisukeosako112@gmail.com>
2023-12-07 12:08:50 +0900

    Futter menu配置(actionはまだ)

--- ContentView.swift
+++ ContentView.swift
@@ -2,42 +2,35 @@ import SwiftUI
 
 struct FooterMenuView: View {
     var body: some View {
-        ZStack {
-            // 画面のコンテンツビュー
-            ContentView()
-            
-            // 画面下部に固定されたフッターメニュー
-            VStack {
+        VStack {
+            Spacer()
+            HStack {
                 Spacer()
-                HStack {
-                    Spacer()
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        Image(systemName: "trash")
-                            .imageScale(/*@START_MENU_TOKEN@*/.large/*@END_MENU_TOKEN@*/)
-                            .foregroundStyle(Color.red)
-                    })
-                    Spacer()
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
-                            .foregroundStyle(Color.red)
-                    })
-                    Spacer()
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
-                            .foregroundStyle(Color.red)
-                    })
-                    Spacer()
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
-                            .foregroundStyle(Color.red)
-                    })
-                    Spacer()
-                }
-                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-                .padding(.vertical, 25)
-                .background(Color.black)
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Image(systemName: "trash")
+                        .imageScale(/*@START_MENU_TOKEN@*/.large/*@END_MENU_TOKEN@*/)
+                        .foregroundStyle(Color.red)
+                })
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+                        .foregroundStyle(Color.red)
+                })
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+                        .foregroundStyle(Color.red)
+                })
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+                        .foregroundStyle(Color.red)
+                })
+                Spacer()
             }
             .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+            .padding(.vertical, 25)
+            .background(Color.black)
         }
         .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
     }
@@ -45,6 +38,7 @@ struct FooterMenuView: View {
 
 struct ContentView: View {
     @State private var isTextVisible = true
+//    @State private var isTextVisible = false
     @State private var textOpacity: Double = 0.0
     
     var body: some View {
@@ -62,21 +56,29 @@ struct ContentView: View {
                         }
                     }
             } else {
-                MultiDatePicker(/*@START_MENU_TOKEN@*/"Label"/*@END_MENU_TOKEN@*/, selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Binding<Set<DateComponents>>@*/.constant([])/*@END_MENU_TOKEN@*/)
-                    .padding(/*@START_MENU_TOKEN@*/.all, 30.0/*@END_MENU_TOKEN@*/)
-                    .opacity(textOpacity)
-                    .onAppear {
-                        withAnimation(
-                            Animation.easeInOut(duration: 1.0)
-                        ) {
-                            textOpacity = 1.0
-                        }
+                ZStack {
+                    VStack {
+                        MultiDatePicker(/*@START_MENU_TOKEN@*/"Label"/*@END_MENU_TOKEN@*/, selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Binding<Set<DateComponents>>@*/.constant([])/*@END_MENU_TOKEN@*/)
+                        
+                        Spacer()
+                            .frame(width: .infinity, height: /*@START_MENU_TOKEN@*/70.0/*@END_MENU_TOKEN@*/)
+                        
                     }
+                    
+                    FooterMenuView()
+                }
+                .opacity(textOpacity)
+                .onAppear {
+                    withAnimation(
+                        Animation.easeInOut(duration: 1.0)
+                    ) {
+                        textOpacity = 1.0
+                    }
+                }
             }
         }
 }
 
 #Preview {
-    //ContentView()
-    FooterMenuView()
+    ContentView()
 }
